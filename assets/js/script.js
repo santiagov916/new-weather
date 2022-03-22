b84dab2b531dbd9e8843f1b8d998e0e939515d77
Santiago Venegas <santiagovenegas@Santiagos-MacBook-Pro.local>
2022-03-22 06:44:27 -0700

    second api running smooth

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,25 @@ var cityName = document.querySelector('#city');
 var apiWeatherRoot = 'https://api.openweathermap.org';
 var apiKey = 'b0af02ce6d6578e341aee9bf7fa71ce7';
 
+function fetchTheWeather(location) {
+
+    var { lon } = location;
+    var { lat } = location;
+    var city = location.name;
+
+    var apiForecast = `${apiWeatherRoot}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly,alerts&appid=${apiKey}`;
+
+    fetch(apiForecast)
+    .then(function(response) {
+
+        if (response.ok) {
+            response.json().then(function(data) {
+                postCityName.textContent = `${city}`;
+            })
+        }
+    })
+}
+
 function fetchGeoCode(search) {
 
     // variable for the api info that we are looking for, this api will give us the coordinates of any given city name, if not, it'll console.log an error
@@ -22,6 +41,7 @@ function fetchGeoCode(search) {
      if (response.ok) {
        response.json().then(function(data) {
         console.log(data[0]);
+        fetchTheWeather(data[0]);
        });
      } else {
        alert('Error: Location doesn`t exist!');
