080d67cc8e8d65cfdbe6f24e120b4e6a1dc0a2ec
Santiago Venegas <santiagovenegas@Santiagos-MacBook-Pro.local>
2022-03-22 06:37:11 -0700

    first api call for geocoding a city name = success

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,37 @@ var todaysDate = document.querySelector('#todays-date');
 var postCityName = document.querySelector('#post-city');
 var searchBtn = document.querySelector('#search-button');
 var historyContainer = document.querySelector('#history');
+var cityName = document.querySelector('#city');
+
+var apiWeatherRoot = 'https://api.openweathermap.org';
+var apiKey = 'b0af02ce6d6578e341aee9bf7fa71ce7';
+
+function fetchGeoCode(search) {
+
+    // variable for the api info that we are looking for, this api will give us the coordinates of any given city name, if not, it'll console.log an error
+    var apiUrl = `${apiWeatherRoot}/geo/1.0/direct?q=${search}&limit=1&appid=${apiKey}`;
+
+    fetch(apiUrl)
+    .then(function(response) {
+     // request was successful
+     if (response.ok) {
+       response.json().then(function(data) {
+        console.log(data[0]);
+       });
+     } else {
+       alert('Error: Location doesn`t exist!');
+     }
+   })
+}
 
 function startSearch(e) {
-    if (!e.target.)
+    if(!cityName.value) {
+        return;
+    }
+    e.preventDefault();
+    var search = cityName.value.trim();
+    fetchGeoCode(search);
+    cityName.value = '';
 }
 
 searchBtn.addEventListener('click', startSearch);
