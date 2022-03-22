f9cb83b8f90e11f61a7b8f38aa7e005a6c48004a
Santiago Venegas <santiagovenegas@Santiagos-MacBook-Pro.local>
2022-03-22 06:29:52 -0700

    basic global variables added

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// global var
+var historySearch = [];
+var placeForecastCard = document.querySelector('#place-forecast-card');
+var placeTodayContainer = document.querySelector('#put-today-here');
+var todaysDate = document.querySelector('#todays-date');
+var postCityName = document.querySelector('#post-city');
+var searchBtn = document.querySelector('#search-button');
+var historyContainer = document.querySelector('#history');
+
+function startSearch(e) {
+    if (!e.target.)
+}
+
+searchBtn.addEventListener('click', startSearch);
